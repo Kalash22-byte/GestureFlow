535bc6cd22b0b95f92f3a641f72c6c01a4e5f973
Kalash22-byte <kalash22.tech@gmail.com>
2025-06-30 17:21:12 +0530

    Update game_control_gestures.py

--- game_control_gestures/game_control_gestures.py
+++ game_control_gestures/game_control_gestures.py
@@ -2,26 +2,21 @@ import cv2
 import mediapipe as mp
 import pyautogui
 
-# Initialize MediaPipe and drawing utility
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
 hands = mp_hands.Hands(max_num_hands=1, min_detection_confidence=0.7)
 
-# Open the webcam
 cap = cv2.VideoCapture(0)
 
-# Function to detect which fingers are up
 def count_fingers(hand_landmarks):
-    tips = [8, 12, 16, 20]  # Index to pinky tips
+    tips = [8, 12, 16, 20]  
     fingers = []
 
-    # Thumb: compare x position (left or right)
     if hand_landmarks.landmark[4].x < hand_landmarks.landmark[3].x:
         fingers.append(1)
     else:
         fingers.append(0)
 
-    # Other fingers: compare tip.y < pip.y
     for tip in tips:
         if hand_landmarks.landmark[tip].y < hand_landmarks.landmark[tip - 2].y:
             fingers.append(1)
@@ -46,21 +41,19 @@ while True:
         for hand_landmarks in result.multi_hand_landmarks:
             fingers = count_fingers(hand_landmarks)
 
-            # GESTURE → ACTIONS:
-            if fingers == [0, 1, 0, 0, 0]:   # Index finger up
+            if fingers == [0, 1, 0, 0, 0]:   
                 action = "RIGHT"
                 pyautogui.press("right")
-            elif fingers == [1, 0, 0, 0, 0]: # Thumb out (left)
+            elif fingers == [1, 0, 0, 0, 0]:
                 action = "LEFT"
                 pyautogui.press("left")
-            elif sum(fingers) == 5:         # All fingers up
+            elif sum(fingers) == 5:      
                 action = "JUMP"
                 pyautogui.press("up")
-            elif sum(fingers) == 0:         # Fist
+            elif sum(fingers) == 0:       
                 action = "ROLL"
                 pyautogui.press("down")
 
-            # Draw hand landmarks
             mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
 
     if action and action != last_action:
